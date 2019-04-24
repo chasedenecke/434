c51adb8c40eb465d01afc66c532b62da1c30f963
Chase Denecke <chasedenecke@gmail.com>
2019-04-23 19:55:42 -0700

    Added skeleton code

---
+++ q2_1.py
@@ -0,0 +1,13 @@
+class node():
+    def __init__(self, parent):
+        self.left = 
+        self.right = 
+        self.dataIndices = list()
+        self.splitValue = 
+    
+
+'''
+Each node has left and right child and a set of data points that flow to it
+Parent nodes hand indices of data down to each child according to which side of the split value that data point falls on
+
+'''
