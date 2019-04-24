e94eb35bbb2aa3d312ae6d6256d508d131b266fa
Chase Denecke <chasedenecke@gmail.com>
2019-04-23 20:29:35 -0700

    pseudocode added

--- q2_1.py
+++ q2_1.py
@@ -1,13 +1,29 @@
 class node():
     def __init__(self, parent):
+        self.parent = parent
         self.left = 
         self.right = 
         self.dataIndices = list()
         self.splitValue = 
+    def 
     
 
 '''
 Each node has left and right child and a set of data points that flow to it
 Parent nodes hand indices of data down to each child according to which side of the split value that data point falls on
+Node figures out which feature it should represent
+Need a set of discrete feature values
 
+Finding threshold for continuous valued features:
+    - sort indices of data features according to the value of that feature for each data point. Remove redundant values using collections library (see stack overflow post on slack)
+    - for attribute in attributes: # iterate through possible attributes this node could represent
+        - for(i = 1; i < last feature; i++)
+            testThreshold = (sortedFeatureValues[i] + sortedFeatureValues[i+1])/2
+            if entropy(threshold, feature) < minEntropy:
+                bestThreshold = testThreshold
+                minEntropy = currentEntropy
+        if minEntropy < minEntropyForAllFeatures
+        minEntropyForAllFeatures = minEntropy
+        
+    self.feature = feature that had minEntropyForAllFeatures
 '''
