8430ada3d97493805dfdc1e75790f6276aadb3cc
Chase Denecke <chasedenecke@gmail.com>
2019-04-24 18:16:37 -0700

    added determineFeatures function

--- q2_1.py
+++ q2_1.py
@@ -4,50 +4,61 @@ import math
 from statistics import mode
 
 class Node():
-    def __init__(self, dataIndices, splitValue=None, parent=None):
+    def __init__(self, dataIndices, data, depth=1, parent=None):
+        self.dataIndices = dataIndices
+        self.data = data
+        self.depth = depth
+        self.splitValue = None
+        self.feature = None # The index of feature according to which this node splits data points
         self.parent = parent
         self.children = []
-        self.dataIndices = dataIndices
-        self.splitValue = splitValue
-
-    def GetData(self):
-        print(self.dataIndices[0])
-        print(self.dataIndices[1])
 
     def entropy(self, Xtrain):
-        count = [2] # index 0 is number of 1 and index 2 is number of  -1
+        count = [0] * 2 # index 0 is number of 1 and index 2 is number of  -1
         for y_i in range(len(self.dataIndices[0])):
             if Xtrain[y_i][0] == -1:
-                counts[0] += 1
-            counts[1] += 1
+                count[0] += 1 # count[0] = negative examples
+            count[1] += 1 # count[1] = positive examples
 
-            s = sum(count)
+            totalData = sum(count)
 
             H = 0
             if not self.Children:
                 for c in count:
-                    purity = c / len(self.dataIndices[0]))
+                    purity = c / len(self.dataIndices[0])
                     H -= purity * math.log2(purity)
                 return H
             else:
-               return  RemainingEntropyOfChildren(Xtrain, s)
+               return  RemainingEntropyOfChildren(Xtrain, totalData)
 
-    def RemaningEntropyofChildren(self, Xtrain, s):
+    def RemaningEntropyofChildren(self, Xtrain, totalData):
         H = 0
         for node in self.children:
-            H += self.len(node.)/ s * child.entropy(Xtrain)
+            H += self.len(node)/ (totalData * child.entropy(Xtrain))
         return H
+    
+        # sort dataIndices according to value of feature
+        # outer loop: iterate through all features in remaining feature list
+        # inner loop: iterate through possible split values to find the one that minimizes entropy for that feature
+    def determineFeature():
+        print("fuck python indentation")
+        minFeatureEntropy = 1
+        for feature in self.data.T:
+            
 
 class Tree():
     def __init__(self, node):
         self.root = node
 
 def Normalize(X):
-    return (X-X.min(0)) / X.ptp(0);
+    return (X-X.min(0)) / X.ptp(0); # X.min retrieves the smallest value in the whole array. 
+                                    # X - X.min(0) shifts all the values in X so that the smallest value is now 0. 
+                                    # X.ptp(0) is X.max - X.min, 
+                                    # so dividing by that value scales all values in X to between 0 and 1.
 
 def GetNormalData(data):
     nd = Normalize(data)
-    x = nd[:,1:]
+    x = nd[:,1:] # First column is class labels, so don't include that in our X values.
     y = data[:,0]
     return x, y
 
@@ -59,11 +70,8 @@ def main():
     Xtest, Ytest = GetNormalData(train)
 
     Xtrain = np.insert(Xtrain, 0, Ytrain, axis=1)
-    row, col = np.indices((Xtrain.shape[0], Xtrain.shape[1]))
-
-    root = Node((row,col))
-    #root.GetData()
-
+    root = Node([x for x in range(Xtrain.shape[0])], Xtrain, 1)
+    
 main()
 
 
