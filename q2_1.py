8ea55edc644cf9b2f3cdbfa25af3e8633832bf74
Chase Denecke <chasedenecke@gmail.com>
2019-04-25 13:29:47 -0700

    bug fixing

--- q2_1.py
+++ q2_1.py
@@ -10,14 +10,15 @@ class Node():
     def __init__(self, Xtrain, Ytrain, depth=1, parent=None):
         self.Xtrain = Xtrain
         self.Ytrain = Ytrain
-        print("Xtrain = ", self.Xtrain)
         self.depth = depth
         self.splitValue = None
         self.feature = None # The index of feature according to which this node splits data points
         self.parent = parent
         self.children = []
+        self.featureIndex = -1
+        self.infoGain = -1
+        self.col_size = -1
         self.determineFeature()
-
     # Given a split value between 0 and 1 and an columnIndex
     # returns the information gain based off the split.
     def GetInfoGain(self, split, columnIndex):
@@ -28,20 +29,20 @@ class Node():
         # Enumerate through the column we keep track of what index the
         # element less than the split was located at.
         for i, elem in enumerate(column):
-            if elem < split:
+            if elem <= split:
                 leftSplit.append(i)
             else:
                 rightSplit.append(i)
 
         # Purity based off the splits splits
-        col_size = len(column)
-        left_p = len(leftSplit) / col_size
-        right_p = len(rightSplit) / col_size
+        self.col_size = len(column)
+        left_p = len(leftSplit) / self.col_size
+        right_p = len(rightSplit) / self.col_size
 
         # Need to have the current entropy of the
         # node to show the info gained from the split.
         # combining them so current is a list of key values for the class.
-        current_entropy = self.entropy(leftSplit + rightSplit)
+        current_entropy = self.entropy(leftSplit + rightSplit) 
 
         # Return info gained by the given split value.
         return (current_entropy - left_p * self.entropy(leftSplit) -
@@ -58,7 +59,7 @@ class Node():
         neg = class_count[0] / len(data)
         pos = class_count[1] / len(data)
         # Zero case, this might be possible.
-        if class_count[0] == 0 or class_count[1] == 0:
+        if neg == 0 or pos == 0:
             return 0
         # We're all made here.
         neg_v = - neg * math.log2(neg)
@@ -70,15 +71,49 @@ class Node():
         # inner loop: iterate through possible split values to find the one
         # that minimizes entropy for that feature
     def determineFeature(self):
-        print("fuck python indentation")
-        minFeatureEntropy = 1
         for column, feature in enumerate(self.Xtrain.T): # Iterate through columns of data by first transposing data matrix
             sortedFeature = np.unique(feature)
             sortedFeature.sort()
-            print("sortedFeature = ", sortedFeature)
-            for x, y in list(zip(sortedFeature, sortedFeature[1:])):
-                threshold = (x + y)/2
+            for x in sortedFeature[:-1]: # This should technically stop 1 spot before the last element of the array but I am lazy
+                infoGain = self.GetInfoGain(x, column)
+                if infoGain > self.infoGain:
+                    self.featureIndex = column
+                    self.infoGain = infoGain
+                    self.splitValue = x
                 #self.entropy(threshold, column)
+
+    # returns list of all leaf-level descendant's information gain and the number of data points in those leaf nodes
+    def reproduce(self):
+        self.determineFeature()
+
+        # Base case. If the data is purely one category or the other return.
+        # Also return if the depth counter we set at the root node has reached zero.
+        if self.infoGain == 0 or self.depth == 0:
+            return [(self.infoGain, self.col_size)]
+        else:
+            column = self.Xtrain[:, self.featureIndex] # Get Column index
+        leftSplitX = [] # Hold data needed for left split. Holds Y keys for left split.
+        rightSplitX = [] # Hold data for right split. Holds Y keys for right split.
+        leftSplitY = []
+        rightSplitY = []
+
+        # Enumerate through the column we keep track of what index the
+        # element less than the split was located at.
+        for i, elem in enumerate(self.Xtrain.T[self.featureIndex]):
+            if elem <= self.splitValue:
+                leftSplitX.append(self.Xtrain[i])
+                leftSplitY.append(self.Ytrain[i])
+            else:
+                rightSplitX.append(self.Xtrain[i])
+                rightSplitY.append(self.Ytrain[i])
+        
+        # Create child nodes and encourage them to give you grandkids
+        leftChild = Node(leftSplitX, leftSplitY, self.depth - 1, None)
+        rightChild = Node(rightSplitX, rightSplitY, self.depth - 1, None)
+        leftList = leftChild.reproduce()
+        rightList = rightChild.reproduce()
+        return leftList + rightList
+        
 
 class Tree():
     def __init__(self, node):
@@ -104,7 +139,7 @@ def main():
     Xtest, Ytest = GetNormalData(train)
 
     root = Node(Xtrain, Ytrain, 1)
-    print(root.GetInfoGain(.2,2))
+    print(root.reproduce())
 main()
 
 
@@ -134,5 +169,6 @@ Finding threshold for continuous valued features:
     - Entropy function
         - Takes two arguments: split value and column index for feature in question
         - Calculate entropy based on the number of feature values less than the split value
-
+        - How does entropy work for leaf nodes?
+    - 
 '''
