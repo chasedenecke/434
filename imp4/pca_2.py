8df5138c5c7182ad49edaecce2bd9869020dc1c5
Ethan Patterson <epatterson888@hotmail.com>
2019-05-24 19:22:03 -0700

    Kinda works

--- imp4/pca_2.py
+++ imp4/pca_2.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import code
 import sys
+ 
+np.set_printoptions(threshold=sys.maxsize)
 
 class LoadData:
   def __init__(self, FILE):
     File = open(FILE, "r")
-    self.data = np.genfromtxt(File, dtype=np.uint8, delimiter=",")
+    self.data = np.genfromtxt(File, delimiter=",")
 
   def featureCount(self):
     return np.size(self.data, 1)
@@ -37,7 +38,7 @@ class PCA:
     return self.cov
 
   def getEiganValues(self, p=False):
-    eigVal, eigVec = np.linalg.eig(self.cov)
+    eigVal, eigVec = np.linalg.eigh(self.cov)
 
     if p == True:
       for i in range(len(eigVal)):
@@ -50,33 +51,29 @@ class PCA:
     return eigVal, eigVec
 
   def getTopEigenValues(self, eigVal, eigVec, n=10):
-      eigMap = [(eigVal[i], eigVec[i]) for i in range(len(eigVal))]
+      eigMap = [(np.abs(eigVal[i]), eigVec[i]) for i in range(len(eigVal))]
       eigMap.sort(key=lambda x: x[0], reverse=True)
-      return [eigMap[i][0] for i in range(n)], [eigMap[i][1] for i in range(n)]
+      return np.asarray([eigMap[i][0] for i in range(n)]), np.asarray([eigMap[i][1] for i in range(n)])
 
 if __name__ == "__main__":
-  FILE = LoadData("debug-data.txt")
-  #FILE = LoadData("p4-data.txt")
-  np.set_printoptions(threshold=sys.maxsize)
+  FILE = LoadData("p4-data.txt")
+
   pca = PCA(FILE)
   pca.setFeatureMean()
   plt.imshow(np.reshape(pca.featureMean,(28,28)))
   plt.show()
+
   pca.getCovMat()
   eigVal, eigVec  = pca.getEiganValues()
   topTenVal, topTenVec = pca.getTopEigenValues(eigVal, eigVec)
-  # print("topTenVec = ", topTenVec)
+  print(topTenVec.T.shape)
+  print(pca.data.shape)
   newRep = np.matmul(topTenVec, pca.data.T).T
-  print(newRep)
-  '''
-  fig, ax = plt.subplots(nrows=3, ncols=4)
-  for i, row in enumerate(ax):
-    for j, col in enumerate(row):
-      if i == 0 and j == 0:
-        plt.imshow(np.reshape(pca.featureMean,(28,28)))
-      else:
-        plt.imshow(np.reshape(topTenVec[i*4 + j - 1], (28,28)))
+  print(newRep.shape)
+
+  plt.imshow(topTenVec[0].reshape((28, 28)))
   plt.show()
-  print(topTenVal)
-  code.interact(local=locals()) # Start interpreter for debugging
-  '''
+  
+  plt.imshow(newRep[0].reshape((2, 5)))
+  plt.show()
+
